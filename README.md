60dd004ef9945f5d252adfd6047254e28281d008
reynaldpn <reynaldpn@gmail.com>
2019-10-21 21:09:03 +0700

    0.3.0

--- README.md
+++ README.md
@@ -1,31 +1,96 @@
-Welcome to crocodocs!
+# Welcome to crocodocs
 
-This package allows you documentating your source codes in more simple steps.
+Create documentation more comfortably 🤩
 
-## Installation ##
+### Installation
 
-1. If you have not installed Node.js, [install](https://nodejs.org/) it first.
+You only need [Node.js](https://nodejs.org/en/) to run this package
 
-## Usage ##
+### Usage
 
-1. To create an information about script description, create a single comment contains ```CROCOS```, following your script's description.
-2. To create an information about function, create a single comment contains ```CROCOF``` above your function, then next to it add your function's description.
+Comment your lines with
 
-## Example ##
+```//Dev YourDescription``` this will declare a script description with description in the documentation
+
+```//VAR TypeData; VariableName; YourDescription``` this will declare a variable with name, type data and description in the documentation
+
+```//FUN TypeData; FunctionName; YourDescription``` this will declare a function with name, type data and description in the documentation
+
+```//PARAM TypeData; ParameterName; YourDescription``` this will declare a function parameter with name, type data and description in the documentation
+
+Your variable or function with / without parameter comments should be exactly above the variable or the function
+
+### Example
 
 ```
-//CROCOS App delegate script, this script is to be the first script to load when user opened the app
+//DES This is a sample hello-world script in react-native
 
-//CROCOF Setting label content
-@IBAction func setContent( 
-    in label: ActiveLabel,
-) {
-    
+import React from "react"
+
+import {
+    Text,
+    View
+} from "react-native"
+
+export default class HelloWorld extends React.Component {
+    //VAR String; textValue; This variable contains a text that to be shown at the screen
+    textValue = "Hello World"
+
+    render() {
+        return (
+            <View
+                style = {{
+                    alignItems: "center",
+                    flex: 1,
+                    justifyContent: "center"
+                }}
+            >
+                <Text
+                    style = {{
+                        fontSize: 24
+                    }}
+                >
+                    {GetTextValue()}
+                </Text>
+            </View>
+        )
+    }
+
+    //FUN String; GetTextValue(); This function is used to get textValue state
+    //PARAM String; additionalText; Additional text to be added at textValue
+    GetTextValue(additionalText) {
+        return this.state.textValue + additionalText
+    }
 }
 ```
 
-## Commands ##
+### Exporting a documentation
 
-1. ```npx crocodocs run``` to generate the documentation after done editing project. Must run it from the project root path.
-2. ```npx crocodocs clean``` to clean all the documentation pdf files. Must run it from the project root path.
-3. ```npx crocodocs -h``` to see help instructions.
+After done with commenting scripts, to export a documentation just run ```npx crocodocs create``` in command prompt / terminal at project's root path, your documentation should be opened directly after finish exporting, you can also find the file at PROJECT'S_ROOT_PATH/documentation/index.html
+
+### Customizing documentation preferences
+
+Create json file named ```crocodocs_prefs.json``` on the project's root path like this
+
+```
+{
+    "name": "Project Name",
+    "colors": {
+        "navbar": "goldenrod",
+        "date": "#000",
+        "content_container": "dimgray",
+        "title": "rgb(255,255,255)",
+        "list_of_contents_title": "rgb(255,255,255)", 
+        "list_of_contents": "gold",
+        "script_name": "rgb(255,255,255)",
+        "type": "red",
+        "name": "gold",
+        "description": "lightgray",
+        "param_type": "greenyellow",
+        "param_name": "gold",
+        "param_description": "darkgray"
+    }
+}
+```
+
+You can fill the string value of name and colors as you like
--- index.js
+++ index.js
@@ -1,55 +1,232 @@
 #!/usr/bin/env node
 
-const CFonts = require('cfonts')
-const chalk = require('chalk')
 const commander = require('commander')
 const fs = require('fs')
 const open = require('open')
-const PDFDocument = require('pdfkit')
+
+const results = []
+
+const path = require('path')
 
 commander
 .version(require("./package.json").version)
-.description('Crocodic\'s Documentation Tool')
+.description('Easily create a documentation, without the tears 😭')
 
 commander
-.command('run')
-.description('Generate documentation')
-.action(() => run())
-
-commander
-.command('clean')
-.description('Clean all pdf files')
-.action(() => clean())
-
-var docName = ""
+.command('create')
+.description('Create the documentation')
+.action(() => create())
 
 commander.parse(process.argv)
 
-function run() {
-    CFonts.say('CROCODOCS', {
-        font: 'block',              // define the font face
-        align: 'left',              // define text alignment
-        colors: ['cyan'],           // define all colors
-        background: 'transparent',  // define the background color, you can also use `backgroundColor` here as key
-        letterSpacing: 1,           // define letter spacing
-        lineHeight: 1,              // define the line height
-        space: true,                // define if the output text should have empty lines on top and on the bottom
-        maxLength: '5',             // define how many character can be on one line
+function create() {
+    const files = getAllFilesObject('.')
+
+    if(files.length > 0) {
+        readFile(files[files.length - 1])
+    }
+}
+
+function readFile(file) {
+    fs.readFile(file.path, {encoding: 'utf-8'}, function(err, data) {
+        if (err) throw error
+
+        if(file.path != "./index.js") { //DELETE THIS WHEN NOT TESTING
+            let lines = data.split('\n')
+
+            for(let i = 0; i < lines.length; i++) {
+                const lowercasedLine = lines[i].toLowerCase()
+
+                const isHavingData = lowercasedLine.includes("//des") || lowercasedLine.includes("//fun") || lowercasedLine.includes("//var")
+
+                let alreadyRegisteredInIndex = -1
+
+                for(let j = 0; j < results.length; j++) {
+                    if(results[j].filePath == file.path) {
+                        alreadyRegisteredInIndex = j
+
+                        break
+                    }
+                }
+
+                if(isHavingData) {
+                    if(alreadyRegisteredInIndex == -1) {
+                        results.push(
+                            {
+                                filePath: file.path,
+                                description: "",
+                                functions: [],
+                                variables: []
+                            }
+                        )
+                    }
+
+                    alreadyRegisteredInIndex = results.length - 1
+
+                    const pickedResult = results[alreadyRegisteredInIndex]
+
+                    if(lowercasedLine.includes("//des")) {
+                        pickedResult.description = lines[i].trim().substring(6, lines[i].length)
+                    } else if(lowercasedLine.includes("//fun")) {
+                        pickedResult.functions.push({
+                            ...GetNameTypeAndDescription(lines[i]),
+                            parameters: []
+                        })
+
+                        let parameterFoundAtNextLine = 1
+
+                        while(parameterFoundAtNextLine != -1) {
+                            let nextLine = lines[i + parameterFoundAtNextLine]
+
+                            let lowercasedNextLine = nextLine.toLowerCase()
+
+                            if(lowercasedNextLine.includes("//param")) {
+                                pickedResult.functions[pickedResult.functions.length - 1].parameters.push(GetNameTypeAndDescription(nextLine))
+
+                                parameterFoundAtNextLine++
+                            } else {
+                                parameterFoundAtNextLine = -1
+                            }
+                        }
+                    } else if(lowercasedLine.includes("//var")) {
+                        pickedResult.variables.push(GetNameTypeAndDescription(lines[i]))
+                    }
+                }
+            }
+        }
+
+        if(file.nextFile != null) {
+            readFile(file.nextFile)
+        } else {
+            const fs = require('fs')
+
+            if (!fs.existsSync('documentation')){
+                fs.mkdirSync('documentation')
+            }
+
+            const fileName = 'documentation/index.html'
+
+            fs.copyFile(__dirname + '/resources/styles.css', 'documentation/styles.css', (err) => {
+                if (err) throw err
+                
+                fs.copyFile(__dirname + '/resources/documentation-source.html', fileName, (err) => {
+                    if (err) throw err
+    
+                    let data = fs.readFileSync(fileName, 'utf-8')
+    
+                    let newValue = data.replace("var array = []", "var array = " + JSON.stringify(results, null, 4))
+
+                    newValue = newValue.replace("[DATE]", (new Date()).toDateString())
+
+                    if (fs.existsSync('./crocodocs_prefs.json')){
+                        fs.readFile('./crocodocs_prefs.json', {encoding: 'utf-8'}, function(err, data) {
+                            if (err) throw error
+
+                            const json = JSON.parse(data)
+
+                            newValue = newValue.replace("[PROJECT_NAME]", json.name || projectName)
+
+                            if(typeof json.colors === 'object' && json.colors != undefined) {
+                                let styles = fs.readFileSync('documentation/styles.css', 'utf-8')
+
+                                if(json.colors.navbar != undefined) {
+                                    styles = styles.replace("/*navbar*/background-color: goldenrod", "/*navbar*/background-color: " + json.colors.navbar)
+                                }
+
+                                if(json.colors.date != undefined) {
+                                    styles = styles.replace("/*date*/color: black", "/*date*/color: " + json.colors.date)
+                                }
+
+                                if(json.colors.content_container != undefined) {
+                                    styles = styles.replace("/*content_container*/background-color:dimgray", "/*content_container*/background-color: " + json.colors.content_container)
+                                }
+
+                                if(json.colors.title != undefined) {
+                                    styles = styles.replace("/*title*/color: white", "/*title*/color: " + json.colors.title)
+                                }
+
+                                if(json.colors.list_of_contents_title != undefined) {
+                                    styles = styles.replace("/*list_of_contents_title*/color: white", "/*list_of_contents_title*/color: " + json.colors.list_of_contents_title)
+                                }
+
+                                if(json.colors.list_of_contents != undefined) {
+                                    styles = styles.replace("/*list_of_contents*/color: gold", "/*list_of_contents*/color: " + json.colors.list_of_contents)
+                                }
+
+                                if(json.colors.script_name != undefined) {
+                                    styles = styles.replace("/*script_name*/color: white", "/*script_name*/color: " + json.colors.script_name)
+                                }
+
+                                if(json.colors.type != undefined) {
+                                    styles = styles.replace("/*type*/color: red", "/*type*/color: " + json.colors.type)
+                                }
+
+                                if(json.colors.name != undefined) {
+                                    styles = styles.replace("/*name*/color: gold", "/*name*/color: " + json.colors.name)
+                                }
+
+                                if(json.colors.description != undefined) {
+                                    styles = styles.replace("/*description*/color: lightgray", "/*description*/color: " + json.colors.description)
+                                }
+
+                                if(json.colors.param_type != undefined) {
+                                    styles = styles.replace("/*param_type*/color: greenyellow", "/*param_type*/color: " + json.colors.param_type)
+                                }
+
+                                if(json.colors.param_name != undefined) {
+                                    styles = styles.replace("/*param_name*/color: gold", "/*param_name*/color: " + json.colors.param_name)
+                                }
+
+                                if(json.colors.param_description!= undefined) {
+                                    styles = styles.replace("/*param_description*/color: darkgray", "/*param_description*/color: " + json.colors.param_description)
+                                }
+
+                                fs.writeFileSync('documentation/styles.css', styles, 'utf-8')
+                            }
+    
+                            fs.writeFileSync(fileName, newValue, 'utf-8')
+            
+                            open(fileName)
+                        })
+                    } else {
+                        newValue = newValue.replace("[PROJECT_NAME]", path.basename(path.resolve(".")))
+    
+                        fs.writeFileSync(fileName, newValue, 'utf-8')
+        
+                        open(fileName)
+                    }
+                })
+            })
+        }
     })
+}
 
-    console.log(chalk.bold.cyan('Crocodic\'s Documentation Tool\n\n'))
+function GetNameTypeAndDescription(line) {
+    const splittedText = line.split(';')
 
-    let filesPath = getListAllFiles('.')
+    const typeString = splittedText[0].trim()
 
-    filesPath.reverse()
+    const type = typeString.split(" ").length == 2 ? typeString.split(" ")[1] : ""
 
-    let files = []
+    return {
+        name: splittedText[1].trim(),
+        type: type,
+        description: splittedText[2].trim()
+    }
+}
+
+//File Helpers
+
+function getAllFilesObject(dirPath) {
+    const allFilesPath = getListAllFilesPath(dirPath)
+
+    const files = []
 
     let previousFile = null
 
-    for(let i = 0; i < filesPath.length; i++) {
-        let newFile = {
-            path: filesPath[i],
+    for(const path of allFilesPath) {
+        const newFile = {
+            path: path,
             nextFile: previousFile
         }
 
@@ -58,175 +235,19 @@ function run() {
         previousFile = newFile
     }
 
-    if(files.length > 0) {
-        readFile(files[files.length - 1], null)
-    }
+    return files
 }
 
-function clean() {
-    if (fs.existsSync("./Documentation")) {
-        let files = getListAllFiles("./Documentation")
+function getListAllFilesPath(dirPath, files_) {
+    files_ = files_ || []
 
-        for(let i = 0; i < files.length; i++) {
-            if(files[i].endsWith(".pdf")) {
-                fs.unlink(files[i], (err) => {
-                    if (err) {
-                        console.log("failed to delete local image:"+err);
-                    }
-                })
-            }
-        }
-    }
+    const files = fs.readdirSync(dirPath)
 
-    console.log(chalk.bold.red("\nAll PDF files on " + chalk.bold.yellow("'Documentation'") + " directory has been cleaned!\n"))
-}
-
-function generateDocName() {
-    let dirName = "Documentation"
-
-    !fs.existsSync(dirName) && fs.mkdirSync(dirName)
-
-    let index = 0
-
-    let slash = (process.platform == "win32" ? "\\" : "/")
-
-    let name = dirName + slash + index.toString()
-
-    do {
-        name = dirName + slash + (++index).toString()
-    } while (fs.existsSync(name + ".pdf"))
-
-    return name += ".pdf"
-}
-
-function readFile(file, recentDoc) {
-    let doc = recentDoc
-
-    fs.readFile(file.path, {encoding: 'utf-8'}, function(err, data) {
-        if (err) throw error
-
-        let result = {
-            information: "",
-            functions: []
-        }
-
-        let dataArray = data.split('\n')
-
-        for (let index = 0; index < dataArray.length; index++) {
-            let trimmedLine = dataArray[index].trim()
-
-            let prefixForScriptDetection = "crocos"
-            let prefixForFunctionDetection = "crocof"
-
-            if (trimmedLine.toLowerCase().includes(prefixForScriptDetection)) {
-                result.information = result.information + (result.information != "" ? "\n" : "") + trimmedLine.substr(trimmedLine.toLowerCase().indexOf(prefixForScriptDetection) + prefixForScriptDetection.length).trim()
-            }
-            
-            if (trimmedLine.toLowerCase().includes(prefixForFunctionDetection)) {
-                let name = ""
-
-                if(index < dataArray.length - 1) {
-                    let currentIndex = index + 1
-
-                    while(dataArray[currentIndex].indexOf("{") == -1 && currentIndex < dataArray.length - 1) {
-                        name = name + dataArray[currentIndex]
-                        name = name.trim()
-
-                        currentIndex++;
-                    } 
-
-                    name = name + dataArray[currentIndex].substr(0, dataArray[currentIndex].indexOf("{") - 1)
-                }
-
-                let parameters = name.substr(name.indexOf("(")).trim().replace(/\s\s+/g, ' ')
-                parameters = parameters.replace("( ", "(")
-
-                name = name.substr(0, name.indexOf("(")).replace(/ +(?= )/g,'').trim()
-
-                if(name != "") {
-                    let description = trimmedLine.substr(trimmedLine.toLowerCase().indexOf(prefixForFunctionDetection) + prefixForFunctionDetection.length).trim()
-
-                    if(description == "") {
-                        description = "** no description **"
-                    }
-
-                    result.functions.push({
-                        name: name,
-                        parameters: parameters,
-                        description: description
-                    })
-                }
-            }
-        }
-
-        if(result.information != "" || result.functions.length > 0) {
-            let slash = (process.platform == "win32" ? "\\" : "/")
-
-            console.log(chalk.bold.white(process.cwd().split(slash)[process.cwd().split(slash).length - 1] + file.path.substr(1)),chalk.bold.gray(result.information,"\n"))
-            
-            if(doc == null) {
-                doc = new PDFDocument()
-
-                docName = generateDocName()
-
-                doc.pipe(fs.createWriteStream(docName))
-            }
-
-            let filePath = file.path.split(slash)[file.path.split(slash).length - 1]
-
-            if(process.platform == "win32") {
-                filePath = filePath.replace("./", "")
-            }
-
-            doc.fillColor("teal").text("• " + filePath + " - " + process.cwd().split(slash)[process.cwd().split(slash).length - 1] + file.path.substr(1))
-            doc.fillColor("black").text("  " + result.information)
-        }
-
-        if(result.functions.length > 0) {
-            console.log(chalk.bold.yellow("Functions Found :"))
-
-            doc.fillColor("gray").text("\n  Functions :\n\n").fillColor("black")
-
-            for (let i = 0; i < result.functions.length; i++) {
-                console.log(chalk.bold.red(result.functions[i].name), chalk.bold.green(result.functions[i].description))
-
-                doc.fillColor("red").text("  " + result.functions[i].name, {continued: true}).fillColor("blue").text(result.functions[i].parameters)
-                doc.fillColor("black").text("  " + result.functions[i].description)
-                
-                doc.text("\n")
-            }
-
-            console.log()
-        }
-
-        if(file.nextFile != null) {
-            if(result.information != "" || result.functions.length > 0) {
-                doc.text("\n")
-            }
-
-            readFile(file.nextFile, doc)
-        } else {
-            if(doc != null) {
-                console.log(chalk.bold.white("\nDocumentation has been successfully created at ") + chalk.bold.yellow(docName + "\n\n"))
-
-                doc.end()
-            }
-
-            open(docName); 
-        }
-    })
-}
-
-function getListAllFiles (dir, files_) {
-    files_ = files_ || [];
-
-    let files = fs.readdirSync(dir);
-
-    for (var i in files){
-        let name = dir + '/' + files[i]
+    for (const file of files){
+        const name = dirPath + '/' + file
 
         if (fs.statSync(name).isDirectory()){
-            getListAllFiles(name, files_)
+            getListAllFilesPath(name, files_)
         } else {
             files_.push(name)
         }
